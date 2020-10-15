269a1221a95da071d07ebd1b1b8002be8cd9105f
Juan Pablo <jp.cano@uniandes.edu.co>
2020-10-15 13:46:56 -0500

    Aux functions

--- Machine Learning/setup_colab.py
+++ Machine Learning/setup_colab.py
@@ -1,11 +1,12 @@
-from sys import path
 import requests
 import sys
 from tqdm.auto import tqdm
 import numpy as np
+import os
 
 def download_github_content(path, filename, chnksz=1000):
-    url = ""
+    url = f"https://github.com/jpcano1/ISIS_4825_Imagenes_Vision/\
+        raw/main/Machine%20Learning/{path}"
     
     try:
         r = requests.get(url, stream=True)
@@ -14,7 +15,6 @@ def download_github_content(path, filename, chnksz=1000):
         sys.exit()
         
     with open(filename, "wb") as f:
-        
         try:
             total = int(np.ceil(int(r.headers.get("content-length"))/chnksz))
         except:
@@ -24,7 +24,39 @@ def download_github_content(path, filename, chnksz=1000):
 
         for pkg in tqdm(gen, total=total, unit="KB"):
             f.write(pkg)
-            
+
         f.close()
         r.close()
     return
+
+def setup_general():
+    os.makedirs("utils", exist_ok=True)
+    with open("utils/__init__.py", "wb"):
+        pass
+
+    download_github_content("utils/general.py", "utils/general.py")
+    print("General Functions Enabled Successfully")
+
+def setup_workshop_1():
+    setup_general()
+    print("Workshop 1 Enabled Successfully")
+
+def setup_workshop_2():
+    setup_general()
+    print("Workshop 2 Enabled Successfully")
+
+def setup_workshop_3():
+    setup_general()
+    print("Workshop 3 Enabled Successfully")
+
+def setup_workshop_4():
+    setup_general()
+    print("Workshop 4 Enabled Successfully")
+
+def setup_workshop_5():
+    setup_general()
+    print("Workshop 5 Enabled Successfully")
+
+def setup_workshop_6():
+    setup_general()
+    print("Workshop 6 Enabled Successfully")
