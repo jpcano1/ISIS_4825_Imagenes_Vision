a87518d582edb0acfaf7de96b0d40937a254d33d
Juan Pablo <jp.cano@uniandes.edu.co>
2020-10-16 17:52:29 -0500

    Funciones auxiliares

--- Machine Learning/utils/general.py
+++ Machine Learning/utils/general.py
@@ -5,6 +5,7 @@ from tqdm.auto import tqdm
 import zipfile
 import os
 import matplotlib.pyplot as plt
+import zipfile
 
 """
 OS Functions
@@ -24,6 +25,14 @@ def read_listdir(dir_):
         full_dir = create_and_verify(dir_, d)
         full_dirs.append(full_dir)
     return np.sort(full_dirs)
+
+def extract_file(filename):
+    with zipfile.ZipFile(filename) as zfile:
+        print("\nExtrayedo Zip File...")
+        zfile.extractall()
+        print("Eliminando Zip File...")
+        os.remove(filename)
+    return
 
 """
 DataViz Functions
@@ -105,9 +114,39 @@ def download_content(url, chnksz=1000, filename="image.jpg", zip=False):
         r.close()
     
     if zip:
-        with zipfile.ZipFile(filename, "r") as zfile:
-            print("\nExtrayedo Zip File...")
-            zfile.extractall()
-            print("Eliminando Zip File...")
-            os.remove(filename)
+        extract_file(filename)
+    return
+
+def download_file_from_google_drive(id_, filename, size=None,
+                                    chnksz=1000, zip=False):
+    def get_confirm_token(response):
+        for key, value in response.cookies.items():
+            if key.startswith('download_warning'):
+                return value
+        return None
+
+    def save_response_content(response, filename, size=None,
+                              chnksz=1000):
+        with open(filename, "wb") as f:
+            gen = response.iter_content(chunk_size=chnksz)
+            for chunk in tqdm(gen, total=size, unit="KB"):
+                if chunk: # filter out keep-alive new chunks
+                    f.write(chunk)
+            f.close()
+
+    URL = "https://docs.google.com/uc?export=download"
+    session = requests.Session()    
+
+    response = session.get(URL, params={ 'id' : id_ }, stream=True)
+    token = get_confirm_token(response)
+
+    if token:
+        params = { 'id' : id_, 'confirm' : token }
+        response = session.get(URL, params=params, stream=True)
+
+    save_response_content(response, filename, size=size,
+                          chnksz=chnksz)
+    response.close()
+    if zip:
+        extract_file(filename)
     return
